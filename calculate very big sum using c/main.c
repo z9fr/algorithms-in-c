7eee0b47a845d5af54d07a5f81bb19ad7fc9e7f4
d4az <dasithvidanage000@gmail.com>
2020-11-22 08:37:29 +0530

    Add files via upload

---
+++ calculate very big sum using c/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int size;
+    long int sum;
+
+    scanf("%d",&size);
+
+    long int arr[size];
+
+    int f;
+
+    for(int i =0 ; i < size; i++)
+    {
+        scanf("%ld" ,&arr[i]);
+        sum = sum + arr[i];
+    }
+    printf("%ld",sum);
+}
