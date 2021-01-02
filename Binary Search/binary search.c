06955a848547574ab1449c5ea04a916ecc61b7cd
d4az <dasithvidanage000@gmail.com>
2021-01-02 15:41:32 +0530

    Update binary search.c

--- Binary Search/binary search.c
+++ Binary Search/binary search.c
@@ -2,43 +2,55 @@
 #include <stdbool.h>
 #define size 5
 
-int search(int data[size], int sk);
+int binarysearch(int data[], int sk)
+{
+    int first = 0;
+    int last = size - 1;
+    int mid = (last + first) / 2;
+
+    bool falg = false;
+
+    while (first < last && !falg)
+    {
+        if (data[mid] == sk)
+        {
+            return mid;
+            falg == true;
+        }
+
+        else if (data[mid] < sk)
+        {
+            falg == false;
+
+            first = mid + 1;
+            last = last;
+        }
+        else if (data[mid] > sk)
+        {
+           falg == false;
+            first = first;
+            last = mid - 1;
+        }
+
+        mid = (last + first) / 2;
+    }
+    return -1;
+}
 
 int main(void)
 {
-    int arr[5] = {1, 2, 3, 4, 5};
-    int sk = 1;
-    int first = 0;
-    int last = size - 1;
+    int data[size] = {1, 2, 3, 4, 5};
+    int sk;
+    printf("Enter the search key ");
+    scanf("%d", &sk);
 
-    search(arr, sk);
-}
+    int value = binarysearch(data, sk);
 
-int search(int array[size], int sk)
-{
-    int first = 0;
-    int last = size - 1;
-    int middle;
-    middle = (first + last) / 2;
-
-    while (first <= last)
+    if (value != -1)
     {
-        middle = (first + last) / 2;
-        if (array[middle] < sk)
-            first = middle + 1;
-        else if (array[middle] == sk)
-        {
-            printf("%d found at the index ->%d.\n", sk, middle + 1);
-            break;
-        }
-        else
-        {
-            last = middle - 1;
-            middle = (first + last) / 2;
-        }
+        printf("value found %d", value);
     }
-    if (first > last)
-    {
-        printf("Not Found!");
+    else{
+        printf("value not found on the array");
     }
 }
